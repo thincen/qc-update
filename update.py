744b4921d83cf708d530f0416de8e5aea193666c
Thincen <lgx@Thincens-iMac.lan>
2021-11-05 01:13:37 +0800

    feat: pushplus feat: mongodb work qc

--- update.py
+++ update.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 # -*-coding:utf-8 -*-
 
-import requests 
+import requests
+import time
+import os,json
+#import sys
 from datetime import datetime,timezone,timedelta
 from lxml import etree
 
@@ -65,12 +68,57 @@ def save(data):
     f.write(s)
     f.close()
 
+def pushDB(data):
+    url=os.getenv("APIURL")
+    if(url==""):
+        #print('getenv("APIURL") err:',file=sys.stderr,flush=True)
+        pushPlus("getenv('APIURL') err")
+        return
+    header={'Content-Type':'application/json'}
+    for node in data:
+        dbData={}
+        dbData['id']=node['id']
+        dbData['c']=node['val']
+        dbData['name']=node['name']
+        dbData['num']=node['num']
+        dbData['expiration']=int(datetime.strptime(node['date'],'%Y-%m-%d').timestamp())
+        dbData['update']=int(datetime.now().astimezone(timezone(timedelta(hours=8))).timestamp())
+        body=json.dumps(dbData).encode(encoding='utf-8')
+        requests.post(url,headers=header,data=body)
+        time.sleep(1)
+
+def pushPlus(msg):
+    token = os.getenv("PUSHPLUSHTOKEN") #在pushpush网站中可以找到
+    if(token==""):
+        #print('getenv("PUSHPLUSHTOKEN") err:',file=sys.stderr,flush=True)
+        pushPlus("getenv('PUSHPLUSHTOKEN') err")
+        return
+    title= '质控样更新' #改成你要的标题内容
+    #content ='内容' #改成你要的正文内容
+    url = 'http://www.pushplus.plus/send'
+    data = {
+        "token":token,
+        "title":title,
+        "content":msg,
+        "template":"json",
+        "channel":"cp",
+        "webhook":"thincen"
+    }
+    body=json.dumps(data).encode(encoding='utf-8')
+    headers = {'Content-Type':'application/json'}
+    requests.post(url,data=body,headers=headers)
+
 def main():
     keys=["cu","pb","zn","cd","fe","mn","ni","k","na","ca","mg","hg","se","as","六价铬","总铬","油","ph","电导","总磷","氨氮","阴离子","挥发酚","总氮","COD","氮氧化物","硝酸盐"]
+    msg=[]
     for key in keys:
         data = parseRes(fetch(key))
+        time.sleep(2)
         if(len(data)>0):
             save(data)
+            pushDB(data)
+            msg.extend(data)
+    pushPlus(msg)
 
 if __name__ =="__main__":
     main()
